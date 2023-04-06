f927ea11265aafa609d2fccefbfb7b071b779af0
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-04-05 21:16:03 -0300

    lista de documentos

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-var lista = document.querySelectorAll(".lista li");
+const documentos = [
+  { id: 1, nome: "Contrato de Trabalho", status: "concluido" },
+  { id: 2, nome: "Fatura de luz", status: "cancelado" },
+  { id: 3, nome: "Pedido de compra", status: "em-progresso" },
+  { id: 4, nome: "Comprovante de pagamento", status: "concluido" },
+  { id: 5, nome: "Relatório de despesas", status: "pendente" },
+  { id: 6, nome: "Certidão de nascimento", status: "concluido" },
+  { id: 7, nome: "Recibo de aluguel", status: "cancelado" },
+  { id: 8, nome: "Declaração de imposto de renda", status: "em-progresso" },
+  { id: 9, nome: "Carteira de motorista", status: "pendente" },
+  { id: 10, nome: "Extrato bancário", status: "concluido" },
+  { id: 11, nome: "Nota fiscal de serviços", status: "concluido" },
+  { id: 12, nome: "Boletim escolar", status: "em-progresso" },
+  { id: 13, nome: "Atestado médico", status: "pendente" },
+  { id: 14, nome: "Licença ambiental", status: "concluido" },
+  { id: 15, nome: "Autorização de viagem", status: "cancelado" },
+  { id: 16, nome: "Contrato de locação", status: "em-progresso" },
+  { id: 17, nome: "Laudo pericial", status: "pendente" },
+  { id: 18, nome: "Passaporte", status: "concluido" },
+  { id: 19, nome: "Holerite", status: "concluido" },
+  { id: 20, nome: "Contrato social", status: "em-progresso" },
+];
 
-for (var i = 0; i < lista.length; i++) {
-  lista[i].addEventListener("click", function () {
+var list = document.querySelector(".lista");
+var elementList = document.querySelectorAll(".documento");
+
+documentos.forEach((doc) => {
+  const li = document.createElement("li");
+  li.setAttribute("data-status", doc.status);
+  li.classList.add("documento");
+  li.innerHTML = doc.nome;
+  list.appendChild(li);
+});
+
+for (const element of elementList) {
+  element.addEventListener("click", function () {
     this.classList.toggle("selecionado");
   });
 }
